09e663504b16ceeae2b8226f3427fe0e8110b7f7
Paul Balog <paulmbalog@gmail.com>
2019-03-28 18:11:10 -0400

    100% Coverage CartItemTest

--- tests/CartItemTest.java
+++ tests/CartItemTest.java
@@ -27,4 +27,106 @@ public class CartItemTest {
         }
     }
 
+    @Test
+    public void testIncreaseItem() {
+        CartItem itemOne = new CartItem(new Item());
+
+        itemOne.incrementCountByOne();
+
+        assertEquals(itemOne.getCount(), 1);
+    }
+
+    @Test
+    public void testDecreaseItem() {
+        CartItem itemOne = new CartItem(new Item());
+
+        itemOne.incrementCountByOne();
+        itemOne.incrementCountByOne();
+        itemOne.incrementCountByOne();
+        itemOne.decrementCountByOne();
+
+        assertEquals(itemOne.getCount(), 2);
+    }
+
+    @Test
+    public void testSetCountItem() {
+        CartItem itemOne = new CartItem(new Item());
+
+        itemOne.setCount(35);
+
+        assertEquals(itemOne.getCount(), 35);
+    }
+
+    @Test
+    public void testModifyItem() {
+        Item butter = new Item();
+        butter.setName("Butter");
+        butter.setDescription("A dairy product with high butterfat content which is solid when chilled and at room temperature in some regions, and liquid when warmed.");
+        butter.setCost(4);
+
+        CartItem itemOne = new CartItem(new Item());
+
+        itemOne.setItem(butter);
+
+        assertEquals(itemOne.getItem(), butter);
+    }
+
+    @Test
+    public void testHashReflexItem() {
+        Item butter = new Item();
+        butter.setName("Butter");
+        butter.setDescription("A dairy product with high butterfat content which is solid when chilled and at room temperature in some regions, and liquid when warmed.");
+        butter.setCost(4);
+
+        CartItem itemOne = new CartItem(new Item());
+
+        itemOne.setItem(butter);
+
+        assertEquals(itemOne.hashCode(), butter.hashCode());
+    }
+
+    @Test
+    public void testNullEqualsCartItem() {
+        Item butter = new Item();
+        butter.setName("Butter");
+        butter.setDescription("A dairy product with high butterfat content which is solid when chilled and at room temperature in some regions, and liquid when warmed.");
+        butter.setCost(4);
+
+        CartItem itemOne = new CartItem(new Item());
+
+        itemOne.setItem(butter);
+
+        assertFalse(itemOne.equals(null));
+    }
+
+    @Test
+    public void testEqualsReflexCartItem() {
+        Item butter = new Item();
+        butter.setName("Butter");
+        butter.setDescription("A dairy product with high butterfat content which is solid when chilled and at room temperature in some regions, and liquid when warmed.");
+        butter.setCost(4);
+
+        CartItem itemOne = new CartItem(new Item());
+
+        itemOne.setItem(butter);
+
+        assertTrue(itemOne.equals(itemOne));
+    }
+
+    @Test
+    public void testUnequalsCartItem() {
+        Item butter = new Item();
+        butter.setName("Butter");
+        butter.setDescription("A dairy product with high butterfat content which is solid when chilled and at room temperature in some regions, and liquid when warmed.");
+        butter.setCost(4);
+
+        CartItem itemOne = new CartItem(new Item());
+
+        itemOne.setItem(butter);
+
+        CartItem itemTwo = new CartItem(new Item());
+
+        assertFalse(itemOne.equals(itemTwo));
+    }
+
 }
