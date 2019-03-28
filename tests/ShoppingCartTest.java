2d3c57c6a701e15a56a3038c680d988cd7b1af8a
Paul Balog <paulmbalog@gmail.com>
2019-03-28 19:03:32 -0400

    Copy/Pasted the wrong thing, still 100%

--- tests/ShoppingCartTest.java
+++ tests/ShoppingCartTest.java
@@ -1,69 +1,149 @@
-package csc4700;
-
-import java.util.ArrayList;
+import csc4700.CartItem;
+import csc4700.Item;
+import csc4700.ShoppingCart;
 import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class ShoppingCart {
+public class ShoppingCartTest {
 
-    private List<CartItem> cartItems = new ArrayList<CartItem>();
-
-    public void addItem(Item item) {
-        if (item == null) {
-            throw new NullPointerException();
-        }
-
-        CartItem cartItem = findCartItem(item);
-
-        // If there is no CartItem, this is the first time it is being
-        // added to the cart and we need to create the mapping object
-        if (cartItem == null) {
-            cartItem = new CartItem(item);
-            cartItems.add(cartItem);
-        }
-
-        // Items may only be added one at a time in this implementation,
-        // so simply increment the quantity.
-        cartItem.incrementCountByOne();
+    @Test
+    public void nullShoppingCart() {
+        ShoppingCart cart = new ShoppingCart();
+        List<CartItem> cartList = cart.getCartItems();
+        assertEquals(cartList.size(), 0);
     }
 
-    public void deleteItem(Item item) {
-        if (item == null) {
-            throw new NullPointerException();
-        }
+    @Test
+    public void checkCartSize() {
+        ShoppingCart cart = new ShoppingCart();
 
-        CartItem cartItem = findCartItem(item);
+        // create array of products -- match individual Item objects in array
+        Item[] products = new Item[3];
 
-        // If there is no corresponding CartItem object, there is nothing
-        // to delete. Some implementations will choose to throw an exception
-        // in this case, however for this project we'll simply exit (having
-        // met the end goal of not having the item in the cart).
-        if (cartItem == null) {
-            return;
-        }
+        // create butter item
+        Item butter = new Item();
+        butter.setName("Butter");
+        butter.setDescription("A dairy product with high butterfat content which is solid when chilled and at room temperature in some regions, and liquid when warmed.");
+        butter.setCost(3);
+        cart.addItem(butter);
 
-        if (cartItem.getCount() == 1) {
-            // Removing the last of this item, so remove it
-            // fully from the cart
-            cartItems.remove(cartItem);
-        }
-        else {
-            // Only decrement the quantity by one, but since there will still
-            // be at least one of the item desired, leave the CartItem in place.
-            cartItem.decrementCountByOne();
+        // create soda item
+        Item soda = new Item();
+        soda.setName("Soda");
+        soda.setDescription("A drink that typically contains carbonated water, a sweetener, and a natural or artificial flavoring.");
+        soda.setCost(2);
+        cart.addItem(soda);
+        products[1] = soda;
+
+        // create lettuce
+        Item lettuce = new Item();
+        lettuce.setName("Lettuce");
+        lettuce.setDescription("A leafy herbaceous annual or biennial plant in the family Asteraceae grown for its leaves which are used as a salad green.");
+        lettuce.setCost(5);
+        cart.addItem(lettuce);
+        products[2] = lettuce;
+
+        // check size of list versus array
+        List<CartItem> cartList = cart.getCartItems();
+        assertEquals(cartList.size(), products.length);
+    }
+
+    @Test
+    public void findCartItem() {
+        ShoppingCart cart = new ShoppingCart();
+
+        // create array of products -- match individual Item objects in array
+
+        // create butter item
+        Item butter = new Item();
+        butter.setName("Butter");
+        butter.setDescription("A dairy product with high butterfat content which is solid when chilled and at room temperature in some regions, and liquid when warmed.");
+        butter.setCost(3);
+        cart.addItem(butter);
+
+        // create soda item
+        Item soda = new Item();
+        soda.setName("Soda");
+        soda.setDescription("A drink that typically contains carbonated water, a sweetener, and a natural or artificial flavoring.");
+        soda.setCost(2);
+        cart.addItem(soda);
+
+        // create lettuce
+        Item lettuce = new Item();
+        lettuce.setName("Lettuce");
+        lettuce.setDescription("A leafy herbaceous annual or biennial plant in the family Asteraceae grown for its leaves which are used as a salad green.");
+        lettuce.setCost(5);
+        cart.addItem(lettuce);
+
+        // check size of list versus array
+        assertEquals(cart.findCartItem(lettuce), lettuce);
+    }
+
+
+    @Test
+    public void testRemoveItem() {
+        ShoppingCart cart = new ShoppingCart();
+
+        // create lettuce
+        Item lettuce = new Item();
+        lettuce.setName("Lettuce");
+        lettuce.setDescription("A leafy herbaceous annual or biennial plant in the family Asteraceae grown for its leaves which are used as a salad green.");
+        lettuce.setCost(5);
+        cart.addItem(lettuce);
+        cart.addItem(lettuce);
+
+        cart.deleteItem(lettuce);
+        assertEquals(cart.findCartItem(lettuce), lettuce);
+    }
+
+    @Test
+    public void testRemoveLastItem() {
+        ShoppingCart cart = new ShoppingCart();
+
+        // create lettuce
+        Item lettuce = new Item();
+        lettuce.setName("Lettuce");
+        lettuce.setDescription("A leafy herbaceous annual or biennial plant in the family Asteraceae grown for its leaves which are used as a salad green.");
+        lettuce.setCost(5);
+        cart.addItem(lettuce);
+
+        cart.deleteItem(lettuce);
+        assertEquals(cart.findCartItem(lettuce), null);
+    }
+
+
+    @Test
+    public void removeNullItem() {
+        ShoppingCart cart = new ShoppingCart();
+
+        // create lettuce
+        Item lettuce = new Item();
+        lettuce.setName("Lettuce");
+        lettuce.setDescription("A leafy herbaceous annual or biennial plant in the family Asteraceae grown for its leaves which are used as a salad green.");
+        lettuce.setCost(5);
+        cart.addItem(lettuce);
+
+        try {
+            cart.deleteItem(null);
+        } catch (NullPointerException e) {
+            // Expected
         }
     }
 
-    public CartItem findCartItem(Item item) {
-        for (CartItem i : cartItems) {
-            if (i.getItem().equals(item)) {
-                return i;
-            }
-        }
-        return null;
-    }
+    @Test
+    public void removeEmptyItem() {
+        ShoppingCart cart = new ShoppingCart();
 
-    public List<CartItem> getCartItems() {
-        return cartItems;
+        // create lettuce
+        Item lettuce = new Item();
+        lettuce.setName("Lettuce");
+        lettuce.setDescription("A leafy herbaceous annual or biennial plant in the family Asteraceae grown for its leaves which are used as a salad green.");
+        lettuce.setCost(5);
+
+        cart.deleteItem(lettuce);
+
+        assertNull(cart.findCartItem(lettuce));
+
     }
 }
-
